ce6c82a7cb59d5fe463964d20694999be96760fc
texora <ultantaylor@gmail.com>
2022-12-22 11:19:15 +0300

    refined favorites view

--- src/components/Favorites/Favorites.tsx
+++ src/components/Favorites/Favorites.tsx
@@ -9,20 +9,51 @@ export const Favorites = () => {
   const { state } = useFavorite()
   const { data: products } = useFetchItemsQuery()
 
+  console.log(state.length, state, state.length === 1)
+
+  // if there are no items in favorites.
+  if (state.length === 1) {
+    return (
+      <Flex
+        flexDir={'column'}
+        justifyContent={'center'}
+        minH='75vh'
+        alignItems={'center'}
+        pt='2rem'
+      >
+        <Text
+          fontSize={['xl', '3xl', '5xl', '7xl']}
+          bgGradient={'linear(to-tl, #8A2387,#E94057,darkorange)'}
+          bgClip={'text'}
+          fontWeight='bold'
+          textAlign={'center'}
+        >
+          It looks rather empty in here :&apos;)
+        </Text>
+        <Text pt='2rem' textAlign={'center'} color={'#E94057'} fontSize={['sm', 'md']} w='80%'>
+          Add some items to favorites!
+        </Text>
+        <Text textAlign={'center'} color={'#E94057'} fontSize={['sm', 'md']} w='80%'>
+          Don&apos;t be shy.
+        </Text>
+      </Flex>
+    )
+  }
+
   return (
     <>
       <>
-        <Box bg='white' rounded={'xl'} minH='85vh'>
-          <Flex justifyContent={'center'} pt='2rem'>
-            <Text
-              fontSize={'7xl'}
-              bgGradient={'linear(to-tl, #8A2387,#E94057,darkorange)'}
-              bgClip={'text'}
-              fontWeight='bold'
-            >
-              Your Picks
-            </Text>
-          </Flex>
+        <Box bg='white' rounded={'xl'} minH='75vh'>
+          <Text
+            textAlign={'center'}
+            pt='2rem'
+            fontSize={['5xl', '7xl']}
+            bgGradient={'linear(to-tl, #8A2387,#E94057,darkorange)'}
+            bgClip={'text'}
+            fontWeight='bold'
+          >
+            Your Picks
+          </Text>
 
           <Flex
             mt='4rem'
@@ -30,7 +61,8 @@ export const Favorites = () => {
             wrap={'wrap'}
             gap='2rem'
             justifyContent={'center'}
-            flexGrow='100%'
+            alignItems={'center'}
+            alignContent='center'
           >
             {products ? (
               state.map((e) => {
