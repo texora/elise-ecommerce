67659dba64fcbff06f96bbbd8cbaba702af77fef
texora <ultantaylor@gmail.com>
2022-12-25 21:57:43 +0300

    updated meta tags

--- src/components/OpenGraphTags.tsx
+++ src/components/OpenGraphTags.tsx
@@ -9,7 +9,9 @@ export const OpenGraphTags = () => {
       <meta property='og:type' content='website' />
       <meta property='og:site_name' content='Elise'></meta>
       <meta property='og:title' content='Elise' />
+      <meta property='og:url' content='https://onurs-ecommerce-site.netlify.app/' />
       <meta property='og:description' content='Products that leave you wanting more!' />
+
       <meta
         property='og:image'
         content='https://cdn.dribbble.com/users/182238/screenshots/14139258/media/114b5ea0abb5e5f7b401126847309cf7.jpg'
