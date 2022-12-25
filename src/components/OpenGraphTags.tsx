2161729b82ab963e03fc5589a3011e51c557c34d
texora <ultantaylor@gmail.com>
2022-12-25 21:49:15 +0300

    updated meta tags

--- src/components/OpenGraphTags.tsx
+++ src/components/OpenGraphTags.tsx
@@ -6,11 +6,13 @@ export const OpenGraphTags = () => {
         content='Great products that leave the competition in dust.'
         key='desc'
       />
+      <meta property='og:type' content='website' />
+      <meta property='og:site_name' content='Elise'></meta>
       <meta property='og:title' content='Elise' />
       <meta property='og:description' content='Products that leave you wanting more!' />
       <meta
         property='og:image'
-        content='https://cdn.dribbble.com/users/182238/screenshots/14139258/media/114b5ea0abb5e5f7b401126847309cf7.jpg?compress=1&resize=400x300&vertical=top'
+        content='https://cdn.dribbble.com/users/182238/screenshots/14139258/media/114b5ea0abb5e5f7b401126847309cf7.jpg'
       />
     </>
   )
