bb15f5fdcd4947cbcd72871971adbf3aa289d1ba
texora <ultantaylor@gmail.com>
2022-12-27 23:09:31 +0300

    adjusted this so that you can't click the navbar buttons when it disappears

--- src/helpers/adjustNavbarOpacityOnScroll.ts
+++ src/helpers/adjustNavbarOpacityOnScroll.ts
@@ -6,7 +6,9 @@ export const adjustNavbarOpacityOnScroll = (display: string) => {
       const navbar = document.getElementById('navbar')!
       if (prevScrollpos < currentScrollPos && display !== 'flex') {
         navbar.style.opacity = '0'
+        navbar.style.visibility = 'hidden'
       } else if (prevScrollpos > currentScrollPos) {
+        navbar.style.visibility = 'visible'
         navbar.style.opacity = '1'
       }
       prevScrollpos = currentScrollPos
