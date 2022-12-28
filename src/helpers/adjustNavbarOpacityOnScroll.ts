152cef10bc14fb59e607959adc808f66ddb65bdd
texora <ultantaylor@gmail.com>
2022-12-28 04:49:38 +0300

    previous commit broke it, fixed it

--- src/helpers/adjustNavbarOpacityOnScroll.ts
+++ src/helpers/adjustNavbarOpacityOnScroll.ts
@@ -5,10 +5,16 @@ export const adjustNavbarOpacityOnScroll = (display: string) => {
       var currentScrollPos = window.pageYOffset
       const navbar = document.getElementById('navbar')!
 
-      if (prevScrollpos < currentScrollPos && display !== 'flex' && currentScrollPos !== 0) {
+      if (prevScrollpos < currentScrollPos && display !== 'flex') {
         navbar.style.opacity = '0'
         navbar.style.visibility = 'hidden'
       } else if (prevScrollpos > currentScrollPos) {
+        navbar.style.visibility = 'visible'
+        navbar.style.opacity = '1'
+      }
+
+      // This prevents navbar from disappearing on mobile when it bumps at the top of the page and scrolls down by itself to fit the view.
+      if (currentScrollPos === 0) {
         navbar.style.visibility = 'visible'
         navbar.style.opacity = '1'
       }
