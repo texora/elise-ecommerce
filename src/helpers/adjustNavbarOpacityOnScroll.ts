bfdf82670d2469394a9a265a21621c2a71ad6dee
texora <ultantaylor@gmail.com>
2022-12-28 04:05:22 +0300

    fixes a bug with navbar disappearing on mobile phones at the top of the page after a fast scroll up

--- src/helpers/adjustNavbarOpacityOnScroll.ts
+++ src/helpers/adjustNavbarOpacityOnScroll.ts
@@ -4,10 +4,17 @@ export const adjustNavbarOpacityOnScroll = (display: string) => {
     window.onscroll = function () {
       var currentScrollPos = window.pageYOffset
       const navbar = document.getElementById('navbar')!
+
       if (prevScrollpos < currentScrollPos && display !== 'flex') {
         navbar.style.opacity = '0'
         navbar.style.visibility = 'hidden'
       } else if (prevScrollpos > currentScrollPos) {
+        navbar.style.visibility = 'visible'
+        navbar.style.opacity = '1'
+      }
+
+      // This prevents navbar from disappearing on mobile when it bumps at the top of the page and scrolls down by itself to fit the view.
+      if (currentScrollPos === 0) {
         navbar.style.visibility = 'visible'
         navbar.style.opacity = '1'
       }
