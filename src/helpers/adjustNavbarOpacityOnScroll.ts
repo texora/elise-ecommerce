aafd282ba66024ddd096a4fd623c04e54fb0d3cf
texora <ultantaylor@gmail.com>
2022-12-28 04:44:32 +0300

    testing if this is a better way to prevent navbar disappearing issue

--- src/helpers/adjustNavbarOpacityOnScroll.ts
+++ src/helpers/adjustNavbarOpacityOnScroll.ts
@@ -5,7 +5,7 @@ export const adjustNavbarOpacityOnScroll = (display: string) => {
       var currentScrollPos = window.pageYOffset
       const navbar = document.getElementById('navbar')!
 
-      if (prevScrollpos < currentScrollPos && display !== 'flex') {
+      if (prevScrollpos < currentScrollPos && display !== 'flex' && currentScrollPos !== 0) {
         navbar.style.opacity = '0'
         navbar.style.visibility = 'hidden'
       } else if (prevScrollpos > currentScrollPos) {
@@ -13,11 +13,6 @@ export const adjustNavbarOpacityOnScroll = (display: string) => {
         navbar.style.opacity = '1'
       }
 
-      // This prevents navbar from disappearing on mobile when it bumps at the top of the page and scrolls down by itself to fit the view.
-      if (currentScrollPos === 0) {
-        navbar.style.visibility = 'visible'
-        navbar.style.opacity = '1'
-      }
       prevScrollpos = currentScrollPos
     }
   }
