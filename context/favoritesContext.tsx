361ce27c03f0130876cfe8eaf788e4fb458cd476
texora <ultantaylor@gmail.com>
2023-02-09 04:18:42 +0300

    added favorites data persistence with localStorage

--- context/favoritesContext.tsx
+++ context/favoritesContext.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 
-export type Action = { type: 'addOrRemove'; id: number }
 export type Dispatch = (action: Action) => void
 export type State = { id: number }[]
+export type Action = { type: 'addOrRemove'; id: number } | { type: 'setFavorites'; favorites: State }
 export type ContextValue = {
   state: State
   dispatch: Dispatch
@@ -20,10 +20,19 @@ const favoriteReducer = (state: State, action: Action) => {
       setTimeout(() => {
         document.getElementById('navbar')!.style.opacity = '1'
       }) //make navbar visible with fading in animation.
+      localStorage.setItem('favorites', JSON.stringify(currentState))
+      return currentState
+    }
+    case 'setFavorites': {
+      let currentState = structuredClone(state)
+      let localFavorites = JSON.parse(localStorage.getItem('favorites')!)
+      if (localFavorites) {
+        currentState = localFavorites
+      }
       return currentState
     }
     default: {
-      throw new Error(`Unhandled action type: ${action.type}`)
+      throw new Error(`Unhandled action type: ${action}`)
     }
   }
 }
@@ -31,6 +40,13 @@ const favoriteReducer = (state: State, action: Action) => {
 export const FavoriteProvider = ({ children }: { children: React.ReactNode }) => {
   const [state, dispatch] = React.useReducer(favoriteReducer, [{ id: 0 }])
   const value = { state, dispatch }
+
+  //get favorites from local storage on first render
+  React.useEffect(() => {
+    let favorites = JSON.parse(localStorage.getItem('favorites')!)
+    if (favorites) dispatch({ type: 'setFavorites', favorites: favorites })
+  }, [])
+
   return <FavoriteContext.Provider value={value}>{children}</FavoriteContext.Provider>
 }
 
